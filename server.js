b5d6cae46ef9899c00487fd2bb115a070620bc08
Ghost <17600079797@163.com>
2019-07-19 18:36:42 +0800

    建设中

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const http = require('http');
 const hostname = '0.0.0.0';
 const port = 80;
 const server = http.createServer((req, res) => {
-    res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain', 'charset=utf-8');
     res.end('神光网站正在建设中...\n');
 });
 
